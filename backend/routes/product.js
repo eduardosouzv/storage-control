9b32855edaa3f63dc742d97cad12e877a28fee72
eduardo <eduardosouzv@gmail.com>
2021-01-19 19:28:22 -0300

    sending products to db

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const pool = require('../connection');
 
 router.post('/register', (req, res) => {
-  res.send({
-    name: req.body.name,
-    quantity: req.body.quantity,
-    price: req.body.price,
+  res.send(
+    (data = {
+      name: req.body.name,
+      quantity: req.body.quantity,
+      price: req.body.price,
+    })
+  );
+
+  pool.query('INSERT INTO produtos(nome, quantidade, preco) VALUES (?,?,?)', [data.name, data.quantity, data.price], (err, result) => {
+    if (err) throw err;
   });
 });
 
